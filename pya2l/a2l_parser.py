40a812bd9ca75f4d1acb18d1e3fb426a8069b91a
Christoph Schueler <cpu12.gems@googlemail.com>
2016-02-27 10:22:11 +0000

    A2L parser now implemented as a class.

--- pya2l/a2l_parser.py
+++ pya2l/a2l_parser.py
@@ -33,6 +33,7 @@ import pya2l.classes as classes
 from pya2l.a2l_lexer import Tokenizer, TokenType
 from pya2l.aml import ParserWrapper
 from pya2l.utils import slicer
+from pya2l.logger import Logger
 
 VERSION = 'ASAM MCD-2MC V1.6'
 
@@ -40,11 +41,11 @@ MAX_IDENT           = 1024
 MAX_PARTIAL_IDENT   = 128
 MAX_STRING          = 255
 
-IDENT_EXPR = r'^(?:[a-zA-Z_][a-zA-Z_0-9]*\.?)+(?:\[(?:\d+ | [a-zA-Z_][a-zA-Z_0-9]*)\])?'
+IDENT_EXPR          = r'^(?:[a-zA-Z_][a-zA-Z_0-9]*\.?)+(?:\[(?:\d+ | [a-zA-Z_][a-zA-Z_0-9]*)\])?'
 
-STATE_NORMAL                = 1
-STATE_COLLECT               = 2
-STATE_A2ML                  = 3
+STATE_NORMAL        = 1
+STATE_COLLECT       = 2
+STATE_A2ML          = 3
 
 from collections import namedtuple
 
@@ -77,119 +78,125 @@ def parseAml(element, level = 0):
         dumpElement(element, level)
 
 
-def a2lParser(fname):
-    keywords = classes.KEYWORD_MAP.keys()
-    fp = codecs.open(fname, encoding = "utf")
+class A2LParser(object):
 
-    source = ''.join(uncomment(fp))
-    tokenizer = Tokenizer(fname, source, keywords)
-
-    classStack = []
-    classStack.append(classes.RootElement)
-
-    instanceStack = []
-    instanceStack.append(classes.instanceFactory("Root"))
-    pushToInstanceStack = False
-
-    while tokenizer.tokenAvailable():
-        lineno, (tokenType, lexem) = tokenizer.getToken()
-
-        if tokenType == TokenType.AML:
-            parserWrapper = aml.ParserWrapper('aml', 'amlFile')
-            tree = parserWrapper.parseFromString(lexem)
-            parseAml(tree.value)
-            continue
-        else:
-            print("[%s]%s:%s" % (tokenType, lexem, lineno))
-
-        if tokenType == TokenType.BEGIN:
-            lineno, (tokenType, lexem) = tokenizer.getToken()   # Move on.
-            pushToInstanceStack = True
-            klass = classes.KEYWORD_MAP.get(lexem)
-            classStack.append(klass)
-        elif tokenType == TokenType.END:
-            lineno, (tokenType, lexem) = tokenizer.getToken()   # Move on.
-            classStack.pop()
-            instanceStack.pop()
-            continue
-        elif tokenType == TokenType.KEYWORD:
-            klass = classes.KEYWORD_MAP.get(lexem)
-
-        if classStack:
-            tos = classStack[-1]
-        if tokenType in (TokenType.BEGIN, TokenType.KEYWORD):
-
-            fixedAttributes =  klass.fixedAttributes
-            variableAttribute =  klass.variableAttribute
-
-            numParameters = len(fixedAttributes)
-
-            parameters = [tokenizer.getToken() for _ in range(numParameters)]
-            attributeValues = [x[1][1] for x in parameters]
-            inst = classes.instanceFactory(lexem.title(), **OrderedDict(zip(fixedAttributes, attributeValues)))
-            if variableAttribute:
-                attr = klass[variableAttribute]
-                result = []
-                while True:
-                    lineno, (tokenType, lexem) = tokenizer.getToken()
-                    print (tokenType, lexem)
-                    if tokenType in (TokenType.KEYWORD, TokenType.END):
-                        tokenizer.stepBack()
-                        break
-                    result.append(lexem)
-                setattr(inst, attr[1], result)
-                inst.attrs.append(attr[1])
-            elif tokenType == TokenType.KEYWORD and lexem in ('COMPU_TAB', 'COMPU_VTAB', 'COMPU_VTAB_RANGE'):
-                #
-                # COMPU_TAB / COMPU_VTAB / COMPU_VTAB_RANGE require special attention.
-                #
-                attribute = "Items"
-                if lexem == 'COMPU_VTAB_RANGE':
-                    sliceLength = 3
-                    valueClass = classes.CompuTriplet
-                    variablePart = [tokenizer.getToken() for _ in range(inst.NumberValueTriples * sliceLength)]
-                else:
-                    valueClass = classes.CompuPair
-                    sliceLength = 2
-                    variablePart = [tokenizer.getToken() for _ in range(inst.NumberValuePairs * sliceLength)]
-                variablePartValues = [v[1][1] for v in variablePart]
-                result = slicer(variablePartValues, sliceLength, valueClass)
-                inst.attrs.append(attribute)
-                setattr(inst, attribute, result)
-            #print inst
-            instanceStack[-1].children.append(inst)
-            if pushToInstanceStack:
-                instanceStack.append(inst)
-                pushToInstanceStack = False
+    def __init__(self):
+        self.logger = Logger(self, 'parser')
 
 
-def uncomment(fp): # Nested comments are not supported!
-    result = []
-    multiLineComment = False
+    def parse(self, filename):
+        keywords = classes.KEYWORD_MAP.keys()
+        fp = codecs.open(filename, encoding = "utf")
 
-    for lineNo, line in enumerate(fp):
-        # Bad style state-machine...
-        if not multiLineComment:
-            if '//' in line:
-                cmtPos = line.index('//')
-                line = line[ : cmtPos].strip()
-                if line:
-                    result.append(line)
-            elif '/*' in line:
-                cmtPos = line.index('/*')
-                startLineNo = lineNo
-                if not '*/' in line:
-                    multiLineComment = True
-                line = line[ : cmtPos].strip()
-                if line:
-                    result.append(line)
+        source = ''.join(self.uncomment(fp))
+        tokenizer = Tokenizer(filename, source, keywords)
+
+        classStack = []
+        classStack.append(classes.RootElement)
+
+        instanceStack = []
+        instanceStack.append(classes.instanceFactory("Root"))
+        pushToInstanceStack = False
+
+        while tokenizer.tokenAvailable():
+            lineno, (tokenType, lexem) = tokenizer.getToken()
+
+            if tokenType == TokenType.AML:
+                parserWrapper = aml.ParserWrapper('aml', 'amlFile')
+                tree = parserWrapper.parseFromString(lexem)
+                parseAml(tree.value)
+                continue
             else:
-                if line:
-                    result.append(line)
-        else:
-            if '*/' in line:
-                cmtPos = line.index('*/')
-                result.append(line[cmtPos + 2: ].strip())
-                multiLineComment = False
-    return result
+                print("[%s]%s:%s" % (tokenType, lexem, lineno))
+
+            if tokenType == TokenType.BEGIN:
+                lineno, (tokenType, lexem) = tokenizer.getToken()   # Move on.
+                pushToInstanceStack = True
+                klass = classes.KEYWORD_MAP.get(lexem)
+                classStack.append(klass)
+            elif tokenType == TokenType.END:
+                lineno, (tokenType, lexem) = tokenizer.getToken()   # Move on.
+                classStack.pop()
+                instanceStack.pop()
+                continue
+            elif tokenType == TokenType.KEYWORD:
+                klass = classes.KEYWORD_MAP.get(lexem)
+
+            if classStack:
+                tos = classStack[-1]
+            if tokenType in (TokenType.BEGIN, TokenType.KEYWORD):
+
+                fixedAttributes =  klass.fixedAttributes
+                variableAttribute =  klass.variableAttribute
+
+                numParameters = len(fixedAttributes)
+
+                parameters = [tokenizer.getToken() for _ in range(numParameters)]
+                attributeValues = [x[1][1] for x in parameters]
+                inst = classes.instanceFactory(lexem.title(), **OrderedDict(zip(fixedAttributes, attributeValues)))
+                if variableAttribute:
+                    attr = klass[variableAttribute]
+                    result = []
+                    while True:
+                        lineno, (tokenType, lexem) = tokenizer.getToken()
+                        print (tokenType, lexem)
+                        if tokenType in (TokenType.KEYWORD, TokenType.END):
+                            tokenizer.stepBack()
+                            break
+                        result.append(lexem)
+                    setattr(inst, attr[1], result)
+                    inst.attrs.append(attr[1])
+                elif tokenType == TokenType.KEYWORD and lexem in ('COMPU_TAB', 'COMPU_VTAB', 'COMPU_VTAB_RANGE'):
+                    #
+                    # COMPU_TAB / COMPU_VTAB / COMPU_VTAB_RANGE require special attention.
+                    #
+                    attribute = "Items"
+                    if lexem == 'COMPU_VTAB_RANGE':
+                        sliceLength = 3
+                        valueClass = classes.CompuTriplet
+                        variablePart = [tokenizer.getToken() for _ in range(inst.NumberValueTriples * sliceLength)]
+                    else:
+                        valueClass = classes.CompuPair
+                        sliceLength = 2
+                        variablePart = [tokenizer.getToken() for _ in range(inst.NumberValuePairs * sliceLength)]
+                    variablePartValues = [v[1][1] for v in variablePart]
+                    result = slicer(variablePartValues, sliceLength, valueClass)
+                    inst.attrs.append(attribute)
+                    setattr(inst, attribute, result)
+                #print inst
+                instanceStack[-1].children.append(inst)
+                if pushToInstanceStack:
+                    instanceStack.append(inst)
+                    pushToInstanceStack = False
+
+
+    def uncomment(self, fp): # Nested comments are not supported!
+        result = []
+        multiLineComment = False
+
+        for lineNo, line in enumerate(fp):
+            # Bad style state-machine...
+            if not multiLineComment:
+                if '//' in line:
+                    cmtPos = line.index('//')
+                    line = line[ : cmtPos].strip()
+                    if line:
+                        result.append(line)
+                elif '/*' in line:
+                    cmtPos = line.index('/*')
+                    startLineNo = lineNo
+                    if not '*/' in line:
+                        multiLineComment = True
+                    line = line[ : cmtPos].strip()
+                    if line:
+                        result.append(line)
+                else:
+                    if line:
+                        result.append(line)
+            else:
+                if '*/' in line:
+                    cmtPos = line.index('*/')
+                    result.append(line[cmtPos + 2: ].strip())
+                    multiLineComment = False
+        return result
 
