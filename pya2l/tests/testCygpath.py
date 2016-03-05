91d746d4f9c814e35b3d8ad3b636f66cdf578662
Christoph Schueler <cpu12.gems@googlemail.com>
2016-03-05 14:39:19 +0100

    Added 'testCygpath'.

---
+++ pya2l/tests/testCygpath.py
@@ -0,0 +1,20 @@
+
+import unittest
+from pya2l.utils import cygpathToWin
+
+
+class TestCygpath(unittest.TestCase):
+
+    def test01(self):
+        self.assertEquals(cygpathToWin("/cygdrive/c/projects/foobar/flonz"), r"c:\projects\foobar\flonz")
+
+    def test02(self):
+        self.assertEquals(cygpathToWin(r"c:\projects\foobar\flonz"), r"c:\projects\foobar\flonz")
+
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
+
