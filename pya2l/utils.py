02fefa31943fd35c4aba46accbd6def031f373c2
Christoph Schueler <cpu12.gems@googlemail.com>
2016-03-18 17:47:49 +0100

    utils.py: Added 'runCommand()'.

--- pya2l/utils.py
+++ pya2l/utils.py
@@ -64,3 +64,49 @@ def cygpathToWin(path):
     return path
 
 
+import ctypes
+
+class StructureWithEnums(ctypes.Structure):
+    """Add missing enum feature to ctypes Structures.
+    """
+    _map = {}
+
+    def __getattribute__(self, name):
+        _map = ctypes.Structure.__getattribute__(self, '_map')
+        value = ctypes.Structure.__getattribute__(self, name)
+        if name in _map:
+            EnumClass = _map[name]
+            if isinstance(value, ctypes.Array):
+                return [EnumClass(x) for x in value]
+            else:
+                return EnumClass(value)
+        else:
+            return value
+
+    def __str__(self):
+        result = []
+        result.append("struct {0} {{".format(self.__class__.__name__))
+        for field in self._fields_:
+            attr, attrType = field
+            if attr in self._map:
+                attrType = self._map[attr]
+            value = getattr(self, attr)
+            result.append("    {0} [{1}] = {2!r};".format(attr, attrType.__name__, value))
+        result.append("};")
+        return '\n'.join(result)
+
+    __repr__ = __str__
+
+
+import subprocess
+
+class CommandError(Exception):
+    pass
+
+def runCommand(cmd):
+    proc = subprocess.Popen(cmd, shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+    result = proc.communicate()
+    proc.wait()
+    if proc.returncode:
+        raise CommandError("{0}".format(result[1]))
+    return result[0]
