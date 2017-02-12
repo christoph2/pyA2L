4f1341c2a1dd3882eb84b312ef11af50cd8e59e6
Christoph Schueler <cpu12.gems@googlemail.com>
2017-02-12 11:23:49 +0100

    Don't generate/use listeners and visitors.

---
+++ pya2l/buildGrammars.cmd
@@ -0,0 +1,3 @@
+java org.antlr.v4.Tool -Dlanguage=Python2 -visitor -Xlog -long-messages aml.g4 -o ./py2/
+java org.antlr.v4.Tool -Dlanguage=Python3 -visitor -Xlog -long-messages aml.g4 -o ./py3/
+  
