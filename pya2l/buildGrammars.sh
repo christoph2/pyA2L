c4aaf930861d9f3bffc9c491a092482dc567eb33
Christoph Schueler <cpu12.gems@googlemail.com>
2017-02-12 07:37:35 +0000

    Add 'buildGrammars.sh'

---
+++ pya2l/buildGrammars.sh
@@ -0,0 +1,4 @@
+#!/bin/env bash
+java org.antlr.v4.Tool -Dlanguage=Python2 -visitor -Xlog -long-messages aml.g4 -o ./py2/
+java org.antlr.v4.Tool -Dlanguage=Python3 -visitor -Xlog -long-messages aml.g4 -o ./py3/
+  
