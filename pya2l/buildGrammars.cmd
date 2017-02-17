ca375c12004c6b3a4e516822a5a0e930415aa1c5
Christoph Schueler <cpu12.gems@googlemail.com>
2017-02-17 14:59:54 +0100

    Update buildGrammars scripts.

--- pya2l/buildGrammars.cmd
+++ pya2l/buildGrammars.cmd
@@ -1,3 +1,5 @@
-java org.antlr.v4.Tool -Dlanguage=Python2 -visitor -Xlog -long-messages aml.g4 -o ./py2/
-java org.antlr.v4.Tool -Dlanguage=Python3 -visitor -Xlog -long-messages aml.g4 -o ./py3/
+java org.antlr.v4.Tool -Dlanguage=Python2 -long-messages aml.g4 -o ./py2/
+java org.antlr.v4.Tool -Dlanguage=Python3 -long-messages aml.g4 -o ./py3/
+java org.antlr.v4.Tool -Dlanguage=Python2 -long-messages -visitor a2l.g4 -o ./py2/
+java org.antlr.v4.Tool -Dlanguage=Python3 -long-messages -visitor a2l.g4 -o ./py3/
   
