8645dbd288eca2440417fde94e1ed4f3910875d7
Christoph Schueler <cpu12.gems@googlemail.com>
2018-11-26 10:35:28 +0100

    Add unicode stuff.

--- pya2l/utils.py
+++ pya2l/utils.py
@@ -110,3 +110,12 @@ def runCommand(cmd):
     if proc.returncode:
         raise CommandError("{0}".format(result[1]))
     return result[0]
+
+from unicodedata import normalize
+
+def nfc_equal(str1, str2):
+    return normalize('NFC', str1) == normalize('NFC', str2)
+
+def fold_equal(str1, str2):
+    return (normalize('NFC', str1).casefold() == normalize('NFC', str2).casefold())
+
