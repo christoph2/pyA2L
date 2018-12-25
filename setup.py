1a41fea4ad019d553dc27d180971209422937a74
Christoph Schueler <cpu12.gems@googlemail.com>
2018-12-25 13:29:59 +0100

    Update requirements

--- setup.py
+++ setup.py
@@ -6,8 +6,14 @@ import sys
 from setuptools import find_packages
 from glob import glob
 
-ANTLR_VERSION = '4.7.1'
+ANTLR_VERSION = '4.7.2'
 ANTLR_RT = "antlr4-python3-runtime=={}".format(ANTLR_VERSION) if sys.version_info.major == 3 else "antlr4-python2-runtime=={}".format(ANTLR_VERSION)
+
+
+install_reqs = [ANTLR_RT, 'mako', 'six']
+
+if sys.version_info.major == 2 or (sys.version_info.major == 3 and sys.version_info.minor < 4):
+    install_reqs.extend(['enum34', 'mock'])
 
 def packagez(base):
     return  ["%s%s%s" % (base, os.path.sep, p) for p in find_packages(base)]
@@ -19,8 +25,8 @@ setup(
     author = 'Christoph Schueler',
     author_email = 'cpu12.gems@googlemail.com',
     url = 'https://www.github.com/Christoph2/pyA2L',
-    packages = ['pya2l'],
-    install_requires = [ANTLR_RT, 'enum34', 'mock', 'mako', 'six'],
+    packages = find_packages(),
+    install_requires = install_reqs,
     #entry_points = {
     #    'console_scripts': [
     #            'vd_exporter = pyA2L.catalogue.vd_exporter:main'
