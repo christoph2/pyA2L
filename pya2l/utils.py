e412b5c3729bfb2946b99fb165ed8063bc36a621
Christoph Schueler <cpu12.gems@googlemail.com>
2019-11-08 12:15:18 +0100

    Add Tristate()

--- pya2l/utils.py
+++ pya2l/utils.py
@@ -4,7 +4,7 @@
 __copyright__ = """
    pySART - Simplified AUTOSAR-Toolkit for Python.
 
-   (C) 2010-2016 by Christoph Schueler <cpu12.gems.googlemail.com>
+   (C) 2010-2019 by Christoph Schueler <cpu12.gems.googlemail.com>
 
    All Rights Reserved
 
@@ -97,6 +97,30 @@ class StructureWithEnums(ctypes.Structure):
 
     __repr__ = __str__
 
+
+class Tristate(object):
+
+    def __init__(self, value=None):
+       if any(value is v for v in (True, False, None)):
+          self.value = value
+       else:
+           raise ValueError("Tristate value must be True, False, or None")
+
+    def __eq__(self, other):
+       return (self.value is other.value if isinstance(other, Tristate)
+               else self.value is other)
+
+    def __ne__(self, other):
+       return not self == other
+
+    def __nonzero__(self):   # Python 3: __bool__()
+       raise TypeError("Tristate object may not be used as a Boolean")
+
+    def __str__(self):
+        return str(self.value)
+
+    def __repr__(self):
+        return "Tristate(%s)" % self.value
 
 import subprocess
 
