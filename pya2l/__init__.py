4c60feb00a1b3e38ab89bf0d5b82a49580204d0f
Christoph Schueler <cpu12.gems@googlemail.com>
2019-12-03 09:49:24 +0100

    Add DB() class

--- pya2l/__init__.py
+++ pya2l/__init__.py
@@ -30,3 +30,59 @@ __version__ = "0.10.2"
 #import pkg_resources
 #pkg_resources.declare_namespace(__name__)
 
+from os import path, unlink
+
+from pya2l.a2l_listener import ParserWrapper, A2LListener, cut_a2ml
+from pya2l.logger import Logger
+
+
+class DB(object):
+    """
+    """
+
+    logger = Logger(__name__)
+
+    def import_a2l(self, file_name, debug = False):
+        """
+        """
+        parser = ParserWrapper('a2l', 'a2lFile', A2LListener, debug = debug)
+
+        self._set_path_components(file_name)
+        try:
+            unlink(self._dbfn)
+        except Exception:
+            pass
+
+        #data = open(fname).read()
+        #data, a2ml = cut_a2ml(data)
+
+        #session = parser.parseFromString(data, dbname = dbfn)
+        #return session
+
+    def export_a2l(self, file_name):
+        """
+        """
+        self._set_path_components(file_name)
+        #raise NotImplementedError("Export functionality not implemented yet.")
+
+    def open(self, file_name):
+        """
+        """
+        self._set_path_components(file_name)
+
+    def exists(self, file_name):
+        """
+        """
+        self._set_path_components(file_name)
+
+    def _set_path_components(self, file_name):
+        """
+        """
+        self._pth, self._base = path.split(file_name)
+        fbase, ext = path.splitext(self._base)
+        self._dbfn = "{}.a2ldb".format(fbase)
+        if not ext or ext.lower() == ".a2l" or ext.lower() == ".a2ldb":
+            self._a2lfn = "{}.a2l".format(fbase)
+        else:
+            self._a2lfn = "{}{}".format(fbase, ext)
+
