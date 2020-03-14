6f5611c38e732bb8bfa24983c41a2728a2db61b4
Christoph Schueler <cpu12.gems@googlemail.com>
2020-03-14 11:20:15 +0100

    Add some semantics

---
+++ pya2l/functions.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__copyright__="""
+    pySART - Simplified AUTOSAR-Toolkit for Python.
+
+   (C) 2009-2020 by Christoph Schueler <cpu12.gems@googlemail.com>
+
+   All Rights Reserved
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+
+"""Functions adding semantics to model elements.
+"""
+
+try:
+    import numpy as np
+except ImportError:
+    pass
+
+try:
+    from scipy.interpolate import RegularGridInterpolator
+except ImportError:
+    pass
+
+from pya2l import math
+
+def compute(compu_method_name):
+    pass
+
+def axis_rescale(no_rescale_x : int, no_axis_pts : int, axis, virtual):
+    """
+    Parameters
+    ----------
+    no_rescale_x: int
+
+    no_axis_pts: int
+        Number of axis-points to calculate; shall be a power of two plus one, e.g. 5, 17, 33...
+
+    axis: list-like
+
+    virtual: list-like
+
+
+    Returns
+    -------
+    list
+    """
+    k = 1
+    d = (virtual[-1] - virtual[0] + 1) // (no_axis_pts - 1)
+    xs = [axis[0]]
+    for idx in range(no_rescale_x - 1):
+        while True:
+            kdv = (k * d) + virtual[0]
+            if kdv >= virtual[idx + 1]:
+                break
+            k += 1
+            x = axis[idx] + (((k - 1) * d) - virtual[idx]) * (axis[idx + 1] - axis[idx]) / (virtual[idx + 1] - virtual[idx])
+            xs.append(x)
+    xs.append(axis[no_rescale_x - 1])
+    return xs
+
+
+class NormalizationAxes:
+    """
+    Appendix C: Using Reference Curves as Normalization Axes for Maps
+
+    Parameters
+    ----------
+    x_norm:
+
+    y_norm:
+
+    z_map
+    """
+
+    def __init__(self, x_norm, y_norm, z_map):
+        self.xn = np.array(x_norm)
+        self.yn = np.array(y_norm)
+        self.zm = np.array(z_map)
+        self.ip_x = math.Interpolate1D(xs = self.xn[:, 0], ys = self.xn[:, 1])
+        self.ip_y = math.Interpolate1D(xs = self.yn[:, 0], ys = self.yn[:, 1])
+        x_size, y_size = self.zm.shape
+        self.ip_m = RegularGridInterpolator((np.arange(x_size), np.arange(y_size)), self.zm, method = "linear")
+
+    def __call__(self, x, y):
+        """
+        Parameters
+        ----------
+        x: float
+
+        y: float
+
+        Returns
+        -------
+        float
+        """
+        x_new = self.ip_x(x)
+        y_new = self.ip_y(y)
+        return self.ip_m((y_new, x_new))
+
---
+++ pya2l/tests/test_functions.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""These test-cases are based on the examples from ASAM MCD-2MC Version 1.6 specification.
+"""
+
+import pytest
+
+from pya2l import functions
+
+try:
+    import numpy as np
+except ImportError:
+    has_numpy = False
+else:
+    has_numpy = True
+
+try:
+    from scipy.interpolate import RegularGridInterpolator
+except ImportError:
+    has_scipy = False
+else:
+    has_scipy = True
+
+
+def test_axis_rescale_ok():
+    EXPECTED = [
+        0, 16.666666666666668, 33.333333333333336, 50.0, 66.66666666666667, 83.33333333333333, 100.0, 158.9206349206349, 216
+    ]
+    assert functions.axis_rescale(no_rescale_x = 3, no_axis_pts = 9,
+        axis = (0x00, 0x64, 0xD8), virtual = (0x00, 0xC0, 0xFF)
+        ) == EXPECTED
+
+X_NORM = (
+    (0.0,       2.0),
+    (200.0,     2.7),
+    (400.0,     3.0),
+    (1000.0,    4.2),
+    (5700,      4.9),
+)
+
+X_IDENT = (
+    (0.0,       0.0),
+    (1.0,       1.0),
+    (2.0,       2.0),
+    (3.0,       3.0),
+    (4.0,       4.0),
+    (5.0,       5.0),
+    (6.0,       6.0),
+)
+
+Y_NORM = (
+    (0.0,       0.5),
+    (50.0,      1.0),
+    (70.0,      2.4),
+    (100.0,     4.2),
+)
+
+Y_IDENT = (
+    (0.0,       0.0),
+    (1.0,       1.0),
+    (2.0,       2.0),
+    (3.0,       3.0),
+    (4.0,       4.0),
+    (5.0,       5.0),
+)
+
+Z_MAP = (
+    (3.4, 4.5, 2.1, 5.4, 1.2, 3.4, 4.4),
+    (2.3, 1.2, 1.2, 5.6, 3.2, 2.1, 7.8),
+    (3.2, 1.5, 3.2, 2.2, 1.6, 1.7, 1.7),
+    (2.1, 0.4, 1.0, 1.5, 1.8, 3.2, 1.5),
+    (1.1, 4.3, 2.1, 4.6, 1.2, 1.4, 3.2),
+    (1.2, 5.3, 3.2, 3.5, 2.1, 1.4, 4.2),
+)
+
+@pytest.mark.skipif("has_numpy == False or has_scipy == False")
+def test_normalization_axes():
+    na = functions.NormalizationAxes(X_NORM, Y_NORM, Z_MAP)
+    assert na(850, 60) == 2.194
+
+@pytest.mark.skipif("has_numpy == False or has_scipy == False")
+def test_normalization_ident():
+    na = functions.NormalizationAxes(X_IDENT, Y_IDENT, Z_MAP)
+    for row_idx, row in enumerate(Z_MAP):
+        for col_idx, value in enumerate(row):
+            assert value == na(col_idx, row_idx)    # Interpolator should just pick every element from Z_MAP.
