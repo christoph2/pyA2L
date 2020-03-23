546a9c575599b41b74ce1fb41ad6a3bdd7044031
Christoph Schueler <cpu12.gems@googlemail.com>
2020-03-23 08:23:44 +0100

    Add test for cse_units.py

--- pya2l/tests/test_various.py
+++ pya2l/tests/test_various.py
@@ -1,9 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: latin-1 -*-
 
 import sys
 
 import pytest
 
 from pya2l import DB
+from pya2l.cse_units import CSE, CSE_Type, Referer
 
 if sys.platform == 'win32':
     FILE_NAME = r"f:\projects\examples\example-a2l-file"
@@ -50,3 +53,6 @@ def test_filename_with_arbitrary_ext():
     assert db._base == r'example-a2l-file.foobar'
     assert db._dbfn == r'example-a2l-file.a2ldb'
     assert db._a2lfn == r'example-a2l-file.foobar'
+
+def test_cse_units():
+    assert CSE.get(2) == CSE_Type(2, "100 µsec", Referer.TIME, "")
