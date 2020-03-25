d3335661418350f39e6e469cf85adc8a253f5067
Christoph Schueler <cpu12.gems@googlemail.com>
2020-03-25 14:13:31 +0100

    Partial CompuMethod implementation

--- pya2l/functions.py
+++ pya2l/functions.py
@@ -26,6 +26,8 @@ __copyright__="""
 """Functions adding semantics to model elements.
 """
 
+from collections import OrderedDict
+
 try:
     import numpy as np
 except ImportError:
@@ -33,13 +35,87 @@ except ImportError:
 
 try:
     from scipy.interpolate import RegularGridInterpolator
+    from scipy import interpolate
 except ImportError:
     pass
 
-from pya2l import math
+from pya2l import exceptions
+from pya2l import model
 
-def compute(compu_method_name):
-    pass
+
+class Interpolate1D:
+    """1-D linear interpolation.
+
+    Parameters
+    ----------
+    xs: iterable of floats
+        X values
+    ys: iterable of floats
+        Y values -- must be of equal length than xs.
+    saturate: bool
+        - ``True``
+            - if X value is less then or equal to min(xs), the result is set to the Y value
+              corresponding to the lowest X value
+            - if X value is greater than or equal to the highest X value, the result is set to the Y value
+              corresponding to the highest X value.
+    """
+    def __init__(self, xs, ys, saturate = True):
+        if any(x1 -x0 <= 0 for x0, x1 in zip(xs, xs[1 : ])):
+            raise ValueError("'xs' must be in strictly increasing order.")
+        self.min_x = min(xs)
+        self.max_x = max(xs)
+
+        if saturate:
+            fill_value = (ys[0], ys[-1])
+            bounds_error = False
+        else:
+            fill_value = None
+            bounds_error = True
+
+        self.interp = interpolate.interp1d(x = xs, y = ys, kind = "linear", bounds_error = bounds_error, fill_value = fill_value)
+
+    def __call__(self, x):
+        """Interpolate a single value.
+
+        Parameters
+        ----------
+        x: float
+        """
+        return float(self.interp(x))
+
+
+class Lookup:
+    """Table lookup
+
+    Parameters
+    ----------
+
+    """
+
+    def __init__(self, xs, ys, saturate = False, y_low = None, y_high = None):
+        if any(x1 -x0 <= 0 for x0, x1 in zip(xs, xs[1 : ])):
+            raise ValueError("'xs' must be in strictly increasing order.")
+        self.xs = xs
+        self.ys = ys
+        self.saturate = saturate
+        self.y_low = y_low
+        self.y_high = y_high
+
+    def __call__(self, x):
+        """
+        """
+        if self.saturate:
+            if x <= self.min_x:
+                return self.y_low or self.ys[0]
+            elif x >= self.max_x:
+                return self.y_high or self.ys[-1]
+        else:
+            if not (self.xs[0] <= x <= self.xs[-1]):
+                raise ValueError("x out of bounds")
+        pos = bisect.bisect_right(self.xs, x) - 1
+        return self.ys[pos]
+
+
 
 def axis_rescale(no_rescale_x : int, no_axis_pts : int, axis, virtual):
     """
@@ -91,8 +167,8 @@ class NormalizationAxes:
         self.xn = np.array(x_norm)
         self.yn = np.array(y_norm)
         self.zm = np.array(z_map)
-        self.ip_x = math.Interpolate1D(xs = self.xn[:, 0], ys = self.xn[:, 1])
-        self.ip_y = math.Interpolate1D(xs = self.yn[:, 0], ys = self.yn[:, 1])
+        self.ip_x = Interpolate1D(xs = self.xn[:, 0], ys = self.xn[:, 1])
+        self.ip_y = Interpolate1D(xs = self.yn[:, 0], ys = self.yn[:, 1])
         x_size, y_size = self.zm.shape
         self.ip_m = RegularGridInterpolator((np.arange(x_size), np.arange(y_size)), self.zm, method = "linear")
 
@@ -113,36 +189,195 @@ class NormalizationAxes:
         return self.ip_m((y_new, x_new))
 
 
-
-class CompuMethod:
-    """
-    Parameters
-    ----------
-    """
-
-    def __init__(self, cm):
-        pass
-
-    def __call__(self):
-        pass
-
-
 class RatFunc:
     """Evaluate rational function.
 
     Parameters
     ----------
-    coeffs: list-like of floats
+    coeffs: object containing coefficients.
         a, b, c, d, e, f - coefficients for the specified formula:
         f(x) = (axx + bx + c) / (dxx + ex + f)
+
+        When using :method:`inv`: Restrictions have to be defined
+        because this general equation cannot always be inverted.
     """
 
     def __init__(self, coeffs):
-        if not (len(coeffs) == 6 and all([isinstance(c, (int, float)) for c in coeffs])):
-            raise TypeError("coeffs must be exactly 6 ints/floats")
-
-        self.p = np.poly1d(coeffs[ : 3])
-        self.q = np.poly1d(coeffs[3 : ])
+        a, b, c, d, e, f = coeffs.a, coeffs.b, coeffs.c, coeffs.d, coeffs.e, coeffs.f
+        self.p = np.poly1d([a, b, c])
+        self.q = np.poly1d([d, e, f])
+        if self.p.order == 1 and self.q.order == 0:
+            self.p_inv = np.poly1d([(f / b), -((f * c) / (b * f))])
+        else:
+            self.p_inv = None
 
     def __call__(self, x):
+        """Evaluate function.
+
+        Parameters
+        ----------
+        x: int or float, scalar or numpy.ndarray
+        """
         return self.p(x) / self.q(x)
+
+    def inv(self, y):
+        """Calculate inverse of function
+        i.e. inv(y = f(x)) ==> x = f_inv(y)
+
+        Parameters
+        ----------
+        y: int or float, scalar or numpy.ndarray
+
+        Note
+        ----
+        Currently inversion of quadratic functions isn't supported, only linear ones.
+        """
+        if self.p.order == 1 and self.q.order == 0:
+            return self.p_inv(y)
+        elif self.p.order == 0 and self.q.order == 0:
+            raise exceptions.MathError("Cannot invert constant function.")
+        else:
+            raise NotImplementedError("Inversion of this kind of polynoms isn't supported yet.")
+
+
+class Identical:
+    """Identity function.
+    """
+
+    def __init__(self):
+        pass
+
+    def __call__(self, x):
+        """Evaluate function.
+
+        Parameters
+        ----------
+        y: int or float
+        """
+        return x
+
+    def inv(self, y):
+        """
+        """
+        return y
+
+
+class Linear:
+    """Evaluate linear function.
+
+    Parameters
+    ----------
+    coeffs: object containing coefficients.
+        a, b - coefficients for the specified formula:
+        coefficients for the specified formula:
+        f(x) = ax + b
+    """
+
+    def __init__(self, coeffs):
+        a, b = coeffs.a, coeffs.b
+        self.p = np.poly1d([a, b])
+
+    def __call__(self, x):
+        """
+        """
+        return self.p(x)
+
+    def _eval_inv(self, y):
+        return (self.p - y).roots[0]
+
+    def inv(self, y):
+        """
+        """
+        if isinstance(y, np.ndarray):
+            return [self._eval_inv(i) for i in y]
+        else:
+            return self._eval_inv(y)
+
+
+class TabVerb:
+    """
+    Parameters
+    ----------
+        mapping: iterable of 2-tuples (key, value)
+            - keys can be either floats or ints (internaly converted to int)
+            - values are strings.
+    """
+
+    def __init__(self, mapping, default = None):
+        mapping = ((int(item[0]), item[1]) for item in mapping)
+        self.mapping = dict(mapping)
+        self.mapping_inv = {v: k for k, v in self.mapping.items()}
+        self.default = default
+
+    def __call__(self, x):
+        """
+        """
+        return self.mapping.get(x, self.default)
+
+    def inv(self, y):
+        """
+        """
+        return self.mapping_inv.get(y)
+
+
+class CompuMethod:
+    """
+    Parameters
+    ----------
+
+    session: Sqlite3 session object
+        Needed for further investigations.
+
+    compu_method: CompuMethod
+    """
+
+    EVALUATORS = {
+        'IDENTICAL' : Identical,
+        'FORM'      : None,
+        'LINEAR'    : Linear,
+        'RAT_FUNC'  : RatFunc,
+        'TAB_INTP'  : None,
+        'TAB_NOINTP': None,
+        'TAB_VERB'  : TabVerb,
+    }
+
+    def __init__(self, session, compu_method: model.CompuMethod):
+        conversionType = compu_method.conversionType
+        if conversionType == "IDENTICAL":
+            self.evaluator = Identical()
+        elif conversionType == "FORM":
+            pass
+        elif conversionType == "LINEAR":
+            coeffs = compu_method.coeffs_linear
+            if coeffs is None:
+                raise exceptions.StructuralError("'LINEAR' requires a coefficients (COEFFS_LINEAR).")
+            self.evaluator = Linear(coeffs)
+        elif conversionType == "RAT_FUNC":
+            coeffs = compu_method.coeffs
+            if coeffs is None:
+                raise exceptions.StructuralError("'RAT_FUNC' requires a coefficients (COEFFS).")
+            self.evaluator = RatFunc(coeffs)
+        elif conversionType == "TAB_INTP":
+            pass
+        elif conversionType == "TAB_NOINTP":
+            pass
+        elif conversionType == "TAB_VERB":
+            table_name = compu_method.compu_tab_ref.conversionTable
+            table = session.query(model.CompuVtab).filter(model.CompuVtab.name == table_name).first()
+            if table is None:
+                raise exceptions.StructuralError("'TAB_VERB' requires a conversation table.")
+            pairs = [(p.inVal, p.outVal) for p in table.pairs]
+            default = table.default_value.display_string if table.default_value else None
+            self.evaluator = TabVerb(pairs, default)
+        else:
+            raise ValueError("Unknown conversation type '{}'.".format(conversionType))
+
+    def __call__(self, x):
+        """
+        """
+        return self.evaluator(x)
+
+    def inv(self, y):
+        """
+        """
+        return self.evaluator.inv(y)
--- pya2l/tests/test_functions.py
+++ pya2l/tests/test_functions.py
@@ -7,6 +7,9 @@
 import pytest
 
 from pya2l import functions
+from pya2l import exceptions
+from pya2l.a2l_listener import ParserWrapper, A2LListener
+from pya2l import model
 
 try:
     import numpy as np
@@ -26,6 +29,34 @@ else:
 RUN_MATH_TEST = has_numpy == True and has_scipy == True
 
 
+class Value:
+    """Value dummy class.
+    """
+
+Xs = [0.0, 200.0, 400.0, 1000.0, 5700.0]
+Ys = [2.0,  2.7, 3.0,  4.2, 4.9]
+Xins = [-1.0, 0.0, 850.0, 5700.0, 8000.0]
+Rs = [2.0, 2.0, 3.9000000000000004, 4.9, 4.9]
+
+@pytest.mark.skipif("RUN_MATH_TEST == False")
+@pytest.mark.parametrize("x, expected", zip(Xins, Rs))
+def test_interpolate1D_saturate(x, expected):
+    interp = functions.Interpolate1D(xs = Xs, ys = Ys, saturate = True)
+    assert interp(x) == expected
+
+
+XsOutOfBounds = [-1.0, 8000.0]
+expected = [None, None]
+
+@pytest.mark.skipif("RUN_MATH_TEST == False")
+@pytest.mark.parametrize("x, expected", zip(XsOutOfBounds, expected))
+def test_interpolate1D_out_of_bounds(x, expected):
+    interp = functions.Interpolate1D(xs = Xs, ys = Ys, saturate = False)
+    with pytest.raises(ValueError):
+        interp(x) == expected
+
+
+@pytest.mark.skipif("RUN_MATH_TEST == False")
 def test_axis_rescale_ok():
     EXPECTED = [
         0, 16.666666666666668, 33.333333333333336, 50.0, 66.66666666666667, 83.33333333333333, 100.0, 158.9206349206349, 216
@@ -91,5 +122,378 @@ def test_normalization_ident():
 
 @pytest.mark.skipif("RUN_MATH_TEST == False")
 def test_ratfunc_identity():
-    rf = functions.RatFunc([0, 1, 0, 0, 0, 1])
+    coeffs = Value()
+    coeffs.a = 0
+    coeffs.b = 1
+    coeffs.c = 0
+    coeffs.d = 0
+    coeffs.e = 0
+    coeffs.f = 1
+    rf = functions.RatFunc(coeffs)
     assert rf(21845) == 21845
+
+@pytest.mark.skipif("RUN_MATH_TEST == False")
+def test_ratfunc_linear():
+    xs = np.arange(-10, 11)
+    ys = np.array(
+        [-6.4, -5.6, -4.8, -4., -3.2, -2.4, -1.6, -0.8, 0., 0.8, 1.6, 2.4, 3.2, 4., 4.8, 5.6, 6.4, 7.2, 8., 8.8, 9.6],
+        dtype = "float"
+    )
+    coeffs = Value()
+    coeffs.a = 0
+    coeffs.b = 4
+    coeffs.c = 8
+    coeffs.d = 0
+    coeffs.e = 0
+    coeffs.f = 5
+    rf = functions.RatFunc(coeffs)
+    assert np.array_equal(rf(xs), ys)
+
+@pytest.mark.skipif("RUN_MATH_TEST == False")
+def test_ratfunc_linear_inv():
+    xs = np.arange(-10, 11)
+    ys = np.array(
+        [-6.4, -5.6, -4.8, -4., -3.2, -2.4, -1.6, -0.8, 0., 0.8, 1.6, 2.4, 3.2, 4., 4.8, 5.6, 6.4, 7.2, 8., 8.8, 9.6],
+        dtype = "float"
+    )
+    coeffs = Value()
+    coeffs.a = 0
+    coeffs.b = 4
+    coeffs.c = 8
+    coeffs.d = 0
+    coeffs.e = 0
+    coeffs.f = 5
+    rf = functions.RatFunc(coeffs)
+    rf = functions.RatFunc(coeffs)
+    assert np.array_equal(rf.inv(ys), xs)
+
+@pytest.mark.skipif("RUN_MATH_TEST == False")
+def test_ratfunc_constant():
+    xs = np.arange(-10, 11)
+    ys = np.full((21,), 10.0)
+    coeffs = Value()
+    coeffs.a = 0
+    coeffs.b = 0
+    coeffs.c = 20
+    coeffs.d = 0
+    coeffs.e = 0
+    coeffs.f = 2
+    rf = functions.RatFunc(coeffs)
+    assert np.array_equal(rf(xs), ys)
+
+@pytest.mark.skipif("RUN_MATH_TEST == False")
+def test_ratfunc_constant_inv():
+    xs = np.arange(-10, 11)
+    ys = np.full((21,), 10.0)
+    coeffs = Value()
+    coeffs.a = 0
+    coeffs.b = 0
+    coeffs.c = 20
+    coeffs.d = 0
+    coeffs.e = 0
+    coeffs.f = 20
+    rf = functions.RatFunc(coeffs)
+    with pytest.raises(exceptions.MathError):
+        rf.inv(ys)
+
+@pytest.mark.skipif("RUN_MATH_TEST == False")
+def test_ratfunc_quadratic():
+    xs = np.arange(-10, 11)
+    ys = np.array([1.231638418079096, 1.1917808219178083, 1.1440677966101696, 1.086021505376344, 1.0140845070422535,
+                   0.9230769230769231, 0.8055555555555556, 0.6521739130434783, 0.46153846153846156,0.3333333333333333,
+                   1.5, 9.0, 6.666666666666667, 4.5, 3.5625, 3.074074074074074, 2.7804878048780486, 2.586206896551724,
+                   2.448717948717949, 2.3465346534653464, 2.267716535433071
+    ])
+    coeffs = Value()
+    coeffs.a = 5
+    coeffs.b = 7
+    coeffs.c = 6
+    coeffs.d = 3
+    coeffs.e = -5
+    coeffs.f = 4
+    rf = functions.RatFunc(coeffs)
+    assert np.array_equal(rf(xs), ys)
+
+@pytest.mark.skipif("RUN_MATH_TEST == False")
+def test_ratfunc_quadratic_inv():
+    xs = np.arange(-10, 11)
+    coeffs = Value()
+    coeffs.a = 5
+    coeffs.b = 7
+    coeffs.c = 6
+    coeffs.d = 3
+    coeffs.e = -5
+    coeffs.f = 4
+    rf = functions.RatFunc(coeffs)
+    with pytest.raises(NotImplementedError):
+        rf.inv(xs)
+
+def test_identical():
+    xs = np.arange(-10, 11)
+    rf = functions.Identical()
+    assert np.array_equal(rf(xs), xs)
+
+def test_identical_inv():
+    xs = np.arange(-10, 11)
+    ys = np.full((21,), 10.0)
+    rf = functions.Identical()
+    assert np.array_equal(rf.inv(xs), xs)
+
+@pytest.mark.skipif("RUN_MATH_TEST == False")
+def test_linear():
+    xs = np.arange(-10, 11)
+    ys = np.array([-43, -39, -35, -31, -27, -23, -19, -15, -11, -7, -3, 1, 5, 9, 13, 17, 21, 25, 29, 33, 37
+    ])
+    coeffs = Value()
+    coeffs.a = 4
+    coeffs.b = -3
+    rf = functions.Linear(coeffs)
+    assert np.array_equal(rf(xs), ys)
+
+@pytest.mark.skipif("RUN_MATH_TEST == False")
+def test_linear_inv():
+    xs = np.arange(-10, 11)
+    ys = np.array([-43, -39, -35, -31, -27, -23, -19, -15, -11, -7, -3, 1, 5, 9, 13, 17, 21, 25, 29, 33, 37
+    ])
+    coeffs = Value()
+    coeffs.a = 4
+    coeffs.b = -3
+    rf = functions.Linear(coeffs)
+    assert np.array_equal(rf.inv(ys), xs)
+
+def test_tab_verb_with_default():
+    mapping = [
+        (1, "SawTooth"),
+        (2, "Square"),
+        (3, "Sinus"),
+    ]
+    default = "unknown signal type"
+    tv = functions.TabVerb(mapping, default = default)
+    assert tv(2) == "Square"
+    assert tv(5) == default
+
+def test_tab_verb_with_default_inv():
+    mapping = [
+        (1, "SawTooth"),
+        (2, "Square"),
+        (3, "Sinus"),
+    ]
+    default = "unknown signal type"
+    tv = functions.TabVerb(mapping, default = default)
+    assert tv.inv("Square") == 2
+    assert tv.inv(default) is None
+
+
+##
+## Basic Integration Tests.
+##
+def test_compu_method_tab_verb():
+    parser = ParserWrapper('a2l', 'module', A2LListener)
+    DATA = """
+    /begin MODULE testModule ""
+        /begin COMPU_METHOD CM.TAB_VERB.DEFAULT_VALUE
+          "Verbal conversion with default value"
+          TAB_VERB "%12.0" ""
+          COMPU_TAB_REF CM.TAB_VERB.DEFAULT_VALUE.REF
+        /end COMPU_METHOD
+        /begin COMPU_VTAB CM.TAB_VERB.DEFAULT_VALUE.REF
+          "List of text strings and relation to impl value"
+          TAB_VERB 3
+          1 "SawTooth"
+          2 "Square"
+          3 "Sinus"
+          DEFAULT_VALUE "unknown signal type"
+        /end COMPU_VTAB
+    /end MODULE
+    """
+    session = parser.parseFromString(DATA)
+    module = session.query(model.Module).first()
+    compu = functions.CompuMethod(session, module.compu_method[0])
+    assert compu(1) == "SawTooth"
+    assert compu.inv("Sinus") == 3
+    assert compu(10) == "unknown signal type"
+
+def test_compu_method_tab_verb_no_default_value():
+    parser = ParserWrapper('a2l', 'module', A2LListener)
+    DATA = """
+    /begin MODULE testModule ""
+        /begin COMPU_METHOD CM.TAB_VERB.DEFAULT_VALUE
+          "Verbal conversion with default value"
+          TAB_VERB "%12.0" ""
+          COMPU_TAB_REF CM.TAB_VERB.DEFAULT_VALUE.REF
+        /end COMPU_METHOD
+        /begin COMPU_VTAB CM.TAB_VERB.DEFAULT_VALUE.REF
+          "List of text strings and relation to impl value"
+          TAB_VERB 3
+          1 "SawTooth"
+          2 "Square"
+          3 "Sinus"
+        /end COMPU_VTAB
+    /end MODULE
+    """
+    session = parser.parseFromString(DATA)
+    module = session.query(model.Module).first()
+    compu = functions.CompuMethod(session, module.compu_method[0])
+    assert compu(1) == "SawTooth"
+    assert compu.inv("Sinus") == 3
+    assert compu(10) is None
+
+def test_compu_method_tab_verb_no_vtab():
+    parser = ParserWrapper('a2l', 'module', A2LListener)
+    DATA = """
+    /begin MODULE testModule ""
+        /begin COMPU_METHOD CM.TAB_VERB.DEFAULT_VALUE
+          "Verbal conversion with default value"
+          TAB_VERB "%12.0" ""
+          COMPU_TAB_REF CM.TAB_VERB.DEFAULT_VALUE.REF
+        /end COMPU_METHOD
+    /end MODULE
+    """
+    session = parser.parseFromString(DATA)
+    module = session.query(model.Module).first()
+    with pytest.raises(exceptions.StructuralError):
+        compu = functions.CompuMethod(session, module.compu_method[0])
+
+def test_compu_method_identical():
+    parser = ParserWrapper('a2l', 'module', A2LListener)
+    DATA = """
+    /begin MODULE testModule ""
+        /begin COMPU_METHOD CM.IDENTICAL
+          "conversion that delivers always phys = int"
+          IDENTICAL "%3.0" "hours"
+        /end COMPU_METHOD
+    /end MODULE
+    """
+    session = parser.parseFromString(DATA)
+    module = session.query(model.Module).first()
+    compu = functions.CompuMethod(session, module.compu_method[0])
+    xs = np.arange(-10, 11)
+    assert np.array_equal(compu(xs), xs)
+    assert np.array_equal(compu.inv(xs), xs)
+
+@pytest.mark.skipif("RUN_MATH_TEST == False")
+def test_compu_method_rat_func_identical():
+    parser = ParserWrapper('a2l', 'module', A2LListener)
+    DATA = """
+    /begin MODULE testModule ""
+        /begin COMPU_METHOD CM.RAT_FUNC.IDENT
+          "rational function with parameter set for int = f(phys) = phys"
+          RAT_FUNC "%3.1" "m/s"
+          COEFFS 0 1 0 0 0 1
+        /end COMPU_METHOD
+    /end MODULE
+    """
+    session = parser.parseFromString(DATA)
+    module = session.query(model.Module).first()
+    compu = functions.CompuMethod(session, module.compu_method[0])
+    xs = np.arange(-10, 11)
+    assert np.array_equal(compu(xs), xs)
+    assert np.array_equal(compu.inv(xs), xs)
+
+@pytest.mark.skipif("RUN_MATH_TEST == False")
+def test_compu_method_rat_func_linear():
+    parser = ParserWrapper('a2l', 'module', A2LListener)
+    DATA = """
+    /begin MODULE testModule ""
+        /begin COMPU_METHOD CM.RAT_FUNC.DIV_81_9175
+          "rational function with parameter set for impl = f(phys) = phys * 81.9175"
+          RAT_FUNC "%8.4" "grad C"
+          COEFFS 0 81.9175 0 0 0 1
+        /end COMPU_METHOD
+    /end MODULE
+    """
+    session = parser.parseFromString(DATA)
+    module = session.query(model.Module).first()
+    compu = functions.CompuMethod(session, module.compu_method[0])
+    xs = np.arange(-10, 11)
+    ys = np.array([-819.1750000000001, -737.2575, -655.34, -573.4225, -491.505, -409.58750000000003, -327.67,
+        -245.7525, -163.835, -81.9175, 0., 81.9175, 163.835, 245.7525, 327.67, 409.58750000000003,
+        491.505, 573.4225, 655.34, 737.2575, 819.1750000000001
+    ])
+    assert np.array_equal(compu(xs), ys)
+    assert np.array_equal(compu.inv(ys), xs)
+
+
+@pytest.mark.skipif("RUN_MATH_TEST == False")
+def test_compu_method_rat_func_no_coeffs():
+    parser = ParserWrapper('a2l', 'module', A2LListener)
+    DATA = """
+    /begin MODULE testModule ""
+        /begin COMPU_METHOD CM.RAT_FUNC.DIV_81_9175
+          "rational function with parameter set for impl = f(phys) = phys * 81.9175"
+          RAT_FUNC "%8.4" "grad C"
+        /end COMPU_METHOD
+    /end MODULE
+    """
+    session = parser.parseFromString(DATA)
+    module = session.query(model.Module).first()
+    with pytest.raises(exceptions.StructuralError):
+        compu = functions.CompuMethod(session, module.compu_method[0])
+
+
+@pytest.mark.skipif("RUN_MATH_TEST == False")
+def test_compu_method_linear():
+    parser = ParserWrapper('a2l', 'module', A2LListener)
+    DATA = """
+    /begin MODULE testModule ""
+        /begin COMPU_METHOD CM.LINEAR.MUL_2
+        "Linear function with parameter set for phys = f(int) = 2*int + 0"
+         LINEAR "%3.1" "m/s"
+         COEFFS_LINEAR 2 0
+        /end COMPU_METHOD
+    /end MODULE
+    """
+    session = parser.parseFromString(DATA)
+    module = session.query(model.Module).first()
+    compu = functions.CompuMethod(session, module.compu_method[0])
+    xs = np.arange(-10, 11)
+    assert np.array_equal(compu(xs), xs * 2.0)
+    assert np.array_equal(compu.inv(xs * 2.0), xs)
+
+@pytest.mark.skipif("RUN_MATH_TEST == False")
+def test_compu_method_linear_no_coeffs():
+    parser = ParserWrapper('a2l', 'module', A2LListener)
+    DATA = """
+    /begin MODULE testModule ""
+        /begin COMPU_METHOD CM.LINEAR.MUL_2
+        "Linear function with parameter set for phys = f(int) = 2*int + 0"
+         LINEAR "%3.1" "m/s"
+        /end COMPU_METHOD
+    /end MODULE
+    """
+    session = parser.parseFromString(DATA)
+    module = session.query(model.Module).first()
+    with pytest.raises(exceptions.StructuralError):
+        compu = functions.CompuMethod(session, module.compu_method[0])
+"""
+
+
+"""
+
+
+"""
+    /begin COMPU_METHOD CM.VTAB_RANGE.DEFAULT_VALUE
+       "verbal range with default value"
+       TAB_VERB
+       "%4.2"
+       ""
+       COMPU_TAB_REF CM.VTAB_RANGE.DEFAULT_VALUE.REF
+    /end COMPU_METHOD
+
+    /begin COMPU_VTAB_RANGE CM.VTAB_RANGE.DEFAULT_VALUE.REF
+       ""
+       11
+       0 1 "Zero_to_one"
+       2 3 "two_to_three"
+       4 7 "four_to_seven"
+       14 17 "fourteen_to_seventeen"
+       18 99 "eigteen_to_ninetynine"
+       100 100 "hundred"
+       101 101 "hundredone"
+       102 102 "hundredtwo"
+       103 103 "hundredthree"
+       104 104 "hundredfour"
+       105 105 "hundredfive"
+       DEFAULT_VALUE "out of range value"
+    /end COMPU_VTAB_RANGE
+"""
