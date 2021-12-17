defb69502ecc490de9ea905937c8e7d8893a3456
Christoph Schueler <cpu12.gems@googlemail.com>
2021-12-17 13:39:48 +0100

    Drop support for Python 3.4

--- setup.py
+++ setup.py
@@ -173,11 +173,12 @@ setup(
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     entry_points = {
         'console_scripts': [
