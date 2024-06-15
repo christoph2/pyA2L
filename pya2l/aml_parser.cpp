3cc6b21c396436a375a598524a60ba564a466dcc
Christoph Schueler <cpu12.gems@googlemail.com>
2024-06-15 12:11:16 +0300

    today()

--- pya2l/aml_parser.cpp
+++ pya2l/aml_parser.cpp
@@ -1,22 +1,20 @@
 #include <iostream>
 
 #include "antlr4-runtime.h"
-#include "amlLexer.h"
-#include "amlParser.h"
-
 #include "aml_visitor.h"
+#include "amlLexer.h"
 
-using namespace std;
+
 using namespace antlr4;
 
 int main(int argc, const char* argv[]) {
 	std::ifstream stream;
-    stream.open("C:/Users/HP/PycharmProjects/pyA2L/examples/ASAP2_Demo_V161.aml");
+    stream.open("C:/csProjects/pyA2L/examples/ASAP2_Demo_V161.aml");
 
 
     ANTLRInputStream input(stream);
 
-    pya2l::amlLexer lexer(&input);
+    amlLexer lexer(&input);
     CommonTokenStream tokens(&lexer);
 #if 0
     while (tokens.LA(1) != Token::EOF) {
@@ -26,8 +24,8 @@ int main(int argc, const char* argv[]) {
     }
 #endif
 
-    pya2l::amlParser parser(&tokens);
-    pya2l::amlParser::AmlFileContext * tree = parser.amlFile();
+    amlParser parser(&tokens);
+    amlParser::AmlFileContext * tree = parser.amlFile();
 
     AmlVisitor visitor;
     auto res = visitor.visitAmlFile(tree);
