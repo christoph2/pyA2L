75afc731d499e35aaaa9e8e5b695d93b080b5d9e
Christoph Schueler <cpu12.gems@googlemail.com>
2024-07-04 19:55:00 +0300

    today()

--- pya2l/aml/unmarshal.cpp
+++ pya2l/aml/unmarshal.cpp
@@ -1,6 +1,7 @@
 
 #include <cassert>
 #include <iostream>
+#include <optional>
 #include <sstream>
 #include <vector>
 
@@ -11,7 +12,7 @@ class Reader {
 
     Reader() = delete;
 
-    Reader(const std::stringstream& inbuf) : m_buf(inbuf.str()) {
+    explicit Reader(const std::stringstream& inbuf) : m_buf(inbuf.str()) {
     }
 
     template<typename T>
@@ -40,86 +41,143 @@ class Reader {
 class Unmarshaller {
    public:
 
+    struct type_t;
+
+    using referrer_t                = std::tuple<ReferrerType, std::string>;
+    using enumerators_t             = std::map<std::string, std::uint32_t>;
+    using enumeration_t             = std::tuple<std::string, enumerators_t>;
+    using enumeration_or_referrer_t = std::variant<std::monostate, enumeration_t, referrer_t>;
+
+    using member_t = std::tuple<std::vector<std::uint32_t>, std::unique_ptr<type_t>>;
+
+    using block_intern_t = std::variant<std::monostate, member_t, std::unique_ptr<type_t>>;
+    using block_t        = std::tuple<std::string, bool, block_intern_t>;
+
+    using tagged_struct_definition_t  = std::tuple<bool, std::optional<std::unique_ptr<type_t>>>;
+    using tagged_struct_member_t      = std::tuple<bool, std::variant<std::monostate, block_t, tagged_struct_definition_t>>;
+    using tagged_struct_t             = std::tuple<std::string, std::vector<std::tuple<std::string, tagged_struct_member_t>>>;
+    using tagged_struct_or_referrer_t = std::variant<std::monostate, tagged_struct_t, referrer_t>;
+
+    using tagged_union_member_t      = std::tuple< std::string, std::variant<std::monostate, block_t, member_t>>;
+    using tagged_union_t             = std::tuple<std::string, std::vector<tagged_union_member_t>>;
+    using tagged_union_or_referrer_t = std::variant<std::monostate, tagged_union_t, referrer_t>;
+
+    using struct_member_t      = std::tuple<bool, member_t>;
+    using struct_t             = std::tuple<std::string, std::vector<struct_member_t>>;
+    using struct_or_referrer_t = std::variant<std::monostate, struct_t, referrer_t>;
+
+    struct type_t {
+        type_t()              = default;
+        type_t(const type_t&) = delete;
+        type_t(type_t&&)      = default;
+
+        type_t(auto&& value) : m_value(std::move(value)) {
+        }
+
+        virtual ~type_t() = default;
+
+        std::variant<
+            std::monostate, AMLPredefinedType, enumeration_or_referrer_t, tagged_struct_or_referrer_t, tagged_union_or_referrer_t,
+            struct_or_referrer_t>
+            m_value;
+    };
+
     Unmarshaller(const std::stringstream& inbuf) : m_reader(inbuf) {
     }
 
-    void load_pdt() {
-        const auto pdt = AMLPredefinedType(m_reader.from_binary<std::uint8_t>());
+    AMLPredefinedType load_pdt() {
+        return AMLPredefinedType(m_reader.from_binary<std::uint8_t>());
     }
 
-    void load_referrrer() {
+    referrer_t load_referrrer() {
         const auto  cat        = ReferrerType(m_reader.from_binary<std::uint8_t>());
         const auto& identifier = m_reader.from_binary_str();
+        return referrer_t{ cat, identifier };
     }
 
-    void load_enum() {
+    enumeration_or_referrer_t load_enum() {
         const auto& disc = m_reader.from_binary_str();
         if (disc == "E") {
             auto name             = m_reader.from_binary_str();
             auto enumerator_count = m_reader.from_binary<std::uint32_t>();
 
+            enumerators_t enumerators;
+
             for (auto idx = 0; idx < enumerator_count; ++idx) {
                 auto tag   = m_reader.from_binary_str();
                 auto value = m_reader.from_binary< std::uint32_t>();
+
+                enumerators.try_emplace(tag, value);
             }
+            return enumeration_t{ name, enumerators };
         } else if (disc == "R") {
-            load_referrrer();
+            return load_referrrer();
         }
     }
 
-    void load_tagged_struct_definition() {
+    tagged_struct_definition_t load_tagged_struct_definition() {
         const auto multiple  = m_reader.from_binary<bool>();
         auto       available = m_reader.from_binary< bool >();
+
         if (available) {
-            load_type();
+            return tagged_struct_definition_t{ multiple, std::make_unique<type_t>(load_type()) };
         }
-        // Else TAG only.
+        // else TAG only.
+        return tagged_struct_definition_t{ multiple, std::nullopt };
     }
 
-    void load_tagged_struct_member() {
+    tagged_struct_member_t load_tagged_struct_member() {
         const auto  multiple = m_reader.from_binary<bool>();
         const auto& dt       = m_reader.from_binary_str();
+
         if (dt == "T") {
-            load_tagged_struct_definition();
+            return tagged_struct_member_t{ multiple, load_tagged_struct_definition() };
         } else if (dt == "B") {
-            load_block();
+            return tagged_struct_member_t{ multiple, load_block() };
         }
     }
 
-    void load_tagged_struct() {
+    tagged_struct_or_referrer_t load_tagged_struct() {
         const auto& disc = m_reader.from_binary_str();
         if (disc == "S") {
-            auto name       = m_reader.from_binary_str();
-            auto tags_count = m_reader.from_binary<std::uint32_t>();
+            auto                                                         name       = m_reader.from_binary_str();
+            auto                                                         tags_count = m_reader.from_binary<std::uint32_t>();
+            std::vector<std::tuple<std::string, tagged_struct_member_t>> members;
             for (auto idx = 0; idx < tags_count; ++idx) {
                 const auto& tag = m_reader.from_binary_str();
-                load_tagged_struct_member();
+                members.emplace_back(tag, load_tagged_struct_member());
             }
+            return tagged_struct_t{ name, std::move(members) };
         } else if (disc == "R") {
-            load_referrrer();
+            return load_referrrer();
         }
     }
 
-    void load_tagged_union() {
+    tagged_union_or_referrer_t load_tagged_union() {
         const auto& disc = m_reader.from_binary_str();
         if (disc == "U") {
-            auto name       = m_reader.from_binary_str();
-            auto tags_count = m_reader.from_binary<std::uint32_t>();
+            auto                               name       = m_reader.from_binary_str();
+            auto                               tags_count = m_reader.from_binary<std::uint32_t>();
+            std::vector<tagged_union_member_t> members;
+
             for (auto idx = 0; idx < tags_count; ++idx) {
                 auto        tag = m_reader.from_binary_str();
                 const auto& dt  = m_reader.from_binary_str();
+
                 if (dt == "M") {
-                    load_member();
+                    members.emplace_back(tag, load_member());
                 } else if (dt == "B") {
-                    load_block();
+                    members.emplace_back(tag, load_block());
                 }
             }
+            return tagged_union_t{ name, std::move(members) };
+
         } else if (disc == "R") {
-            load_referrrer();
+            return load_referrrer();
         }
     }
 
-    void load_type() {
+    type_t load_type() {
         const auto& tag = m_reader.from_binary_str();
         // "PD" - AMLPredefinedType
         // "TS" - TaggedStruct
@@ -127,67 +185,79 @@ class Unmarshaller {
         // "ST" - Struct
         // "EN" - Enumeration
         const auto& disc = m_reader.from_binary_str();
+        type_t      result;
 
         if (disc == "PD") {
-            load_pdt();
+            return load_pdt();
         } else if (disc == "TS") {
-            load_tagged_struct();
+            return load_tagged_struct();
         } else if (disc == "TU") {
-            load_tagged_union();
+            return load_tagged_union();
         } else if (disc == "ST") {
-            load_struct();
+            return load_struct();
         } else if (disc == "EN") {
-            load_enum();
+            return load_enum();
         } else {
             assert(true == false);
         }
+        return result;
     }
 
-    void load_member() {
+    member_t load_member() {
         auto                       arr_count = m_reader.from_binary<std::uint32_t>();
         std::vector<std::uint32_t> array_spec;
         for (auto idx = 0; idx < arr_count; ++idx) {
             array_spec.push_back(m_reader.from_binary<std::uint32_t>());
         }
-        load_type();
+        return member_t{ array_spec, std::make_unique<type_t>(load_type()) };
     }
 
-    void load_struct() {
+    struct_or_referrer_t load_struct() {
         const auto& disc = m_reader.from_binary_str();
         if (disc == "S") {
-            auto name         = m_reader.from_binary_str();
-            auto member_count = m_reader.from_binary<std::uint32_t>();
+            auto                         name         = m_reader.from_binary_str();
+            auto                         member_count = m_reader.from_binary<std::uint32_t>();
+            std::vector<struct_member_t> members;
+
             for (auto idx = 0; idx < member_count; ++idx) {
-                load_member();
-                auto mult = m_reader.from_binary<bool>();
+                auto member = load_member();
+                auto mult   = m_reader.from_binary<bool>();
+                members.emplace_back(mult, std::move(member));
             }
+            return struct_t{ name, std::move(members) };
         } else if (disc == "R") {
-            load_referrrer();
+            return load_referrrer();
         }
     }
 
-    void load_block() {
+    block_t load_block() {
         const auto& tag  = m_reader.from_binary_str();
         const auto& disc = m_reader.from_binary_str();
+
+        block_intern_t result{};
         if (disc == "T") {
-            load_type();
+            result = std::make_unique<type_t>(load_type());
         } else if (disc == "M") {
-            load_member();
+            result = load_member();
         }
         auto multiple = m_reader.from_binary<bool>();
+
+        return block_t{ tag, multiple, std::move(result) };
     }
 
-    void run() {
-        auto decl_count = m_reader.from_binary<std::uint32_t>();
+    std::vector<std::variant<type_t, block_t>> run() {
+        auto                                       decl_count = m_reader.from_binary<std::uint32_t>();
+        std::vector<std::variant<type_t, block_t>> result;
         for (auto idx = 0; idx < decl_count; ++idx) {
             const auto& disc1 = m_reader.from_binary_str();
 
             if (disc1 == "TY") {
-                load_type();
+                result.emplace_back(load_type());
             } else if (disc1 == "BL") {
-                load_block();
+                result.emplace_back(load_block());
             }
         }
+        return result;
     }
 
    private:
@@ -196,6 +266,6 @@ class Unmarshaller {
 };
 
 void unmarshal(const std::stringstream& inbuf) {
-    auto unm = Unmarshaller(inbuf);
-    unm.run();
+    auto unm    = Unmarshaller(inbuf);
+    auto result = unm.run();
 }
