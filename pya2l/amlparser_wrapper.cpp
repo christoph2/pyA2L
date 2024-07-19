5e26d249041bdb3902905d540ab9c7efcaad2d69
Christoph Schueler <cpu12.gems@googlemail.com>
2024-07-19 16:16:58 +0300

    today()

---
+++ pya2l/amlparser_wrapper.cpp
@@ -0,0 +1,22 @@
+#include <pybind11/functional.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+#include <sstream>
+
+namespace py = pybind11;
+
+std::string parse(const std::string& aml_stuff);
+
+PYBIND11_MODULE(amlparser_ext, m) {
+    m.def("parse", &parse);
+#if 0
+    py::class_<ValueContainer>(m, "ValueContainer")
+        .def(py::init<std::string_view>(), py::arg("name"))
+        .def("get_name", &ValueContainer::get_name)
+        .def("get_keywords", &ValueContainer::get_keywords)
+        .def("get_parameters", &ValueContainer::get_parameters)
+        .def("get_multiple_values", &ValueContainer::get_multiple_values)
+        .def("get_if_data", &ValueContainer::get_if_data)
+#endif
+};
