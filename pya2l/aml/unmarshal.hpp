1ee83101b6d4868e6852e6f51d511adb144773cf
Christoph Schueler <cpu12.gems@googlemail.com>
2024-08-02 19:17:15 +0300

    Update grammar

--- pya2l/aml/unmarshal.hpp
+++ pya2l/aml/unmarshal.hpp
@@ -44,7 +44,7 @@ class Reader {
 };
 
 class Node {
-public:
+   public:
 
     enum class NodeType : std::uint8_t {
         TERMINAL,
@@ -77,36 +77,163 @@ public:
     };
 
     using terminal_t = std::variant<std::monostate, long long, float, std::string>;
-    using list_t = std::vector<Node>;
-    using map_t = std::map<std::string, Node>;
+    using list_t     = std::vector<Node>;
+    using map_t      = std::map<std::string, Node>;
 
-    explicit Node() : m_aml_type(AmlType::NONE), m_node_type(NodeType::NONE) {}
-
-    explicit Node(AmlType aml_type, const terminal_t value) : m_aml_type(aml_type), m_node_type(NodeType::TERMINAL),
-        m_value(value) {
-
+    explicit Node() : m_aml_type(AmlType::NONE), m_node_type(NodeType::NONE) {
     }
 
-    explicit Node(AmlType aml_type, const list_t& list) : m_aml_type(aml_type), m_node_type(NodeType::AGGR), m_list(list) {}
-    explicit Node(AmlType aml_type, const map_t& map) : m_aml_type(aml_type), m_node_type(NodeType::MAP), m_map(map) {}
+    explicit Node(AmlType aml_type, const terminal_t value) :
+        m_aml_type(aml_type), m_node_type(NodeType::TERMINAL), m_value(value) {
+    }
 
-private:
-    AmlType m_aml_type;
-    NodeType m_node_type;
+    explicit Node(AmlType aml_type, const list_t& list) : m_aml_type(aml_type), m_node_type(NodeType::AGGR), m_list(list) {
+    }
+
+    explicit Node(AmlType aml_type, const map_t& map) : m_aml_type(aml_type), m_node_type(NodeType::MAP), m_map(map) {
+    }
+
+    const Node* find_block(const std::string& name) const noexcept {
+        if (m_aml_type == AmlType::BLOCK) {
+            if (m_map.contains("TAG")) {
+                if (get_tag() == name) {
+                    return this;
+                } else {
+                    return nullptr;
+                }
+            } else {
+                return nullptr;
+            }
+        } else {
+            for (auto entry : get_members()) {
+                std::cout << static_cast<int>(entry->node_type()) << " " << static_cast<int>(entry->aml_type()) << std::endl;
+                if (entry->aml_type() == Node::AmlType::BLOCK) {
+                    if (entry->get_tag() == name) {
+                        return entry;
+                    }
+                }
+            }
+        }
+        return nullptr;
+    }
+
+    std::tuple<bool, std::optional<const Node*>, std::optional<const Node*>> member_or_type() const noexcept {
+        if (m_aml_type == AmlType::BLOCK) {
+            auto        multiple = is_multiple();
+            const auto& member   = m_map.at("MEMBER");
+            if (member.aml_type() == Node::AmlType::NONE) {
+                const auto& type = m_map.at("TYPE");
+                return { multiple, std::nullopt, &type };
+            } else {
+                return { multiple, &member, std::nullopt };
+            }
+        }
+        return { false, {}, {} };
+    }
+
+    ///////////////////////////////////////////////////////////////
+    const Node* find_tag(const std::string& tag) const noexcept {
+        const auto res = get_tag();
+
+        for (const auto& member : get_members()) {
+            auto mt = member->get_tag();
+            if (mt == tag) {
+                return member;
+            }
+        }
+        return nullptr;
+    }
+
+    std::string get_tag() const noexcept {
+        if (m_node_type == NodeType::MAP) {
+            if (m_map.contains("TAG")) {
+                const auto& tag = m_map.at("TAG");
+                return std::get<std::string>(tag.value());
+            } else {
+                return {};
+            }
+        }
+        return {};
+    }
+
+    bool is_multiple() const noexcept {
+        if (m_node_type == NodeType::MAP) {
+            const auto& multiple = m_map.at("MULTIPLE");
+            return bool(std::get<long long>(multiple.value()));
+        }
+        return false;
+    }
+
+    std::tuple<std::vector<long long>, const Node*> get_type() const noexcept {
+        if (m_node_type == NodeType::MAP) {
+            if (m_map.contains("MEMBER")) {
+                std::vector<long long> arr_spec;
+                const auto&            member = m_map.at("MEMBER");
+                for (const auto& elem : member.map().at("ARR_SPEC").list()) {
+                    arr_spec.push_back(std::get<long long>(elem.value()));
+                }
+                const auto& type = member.map().at("TYPE");
+                return { arr_spec, &type };
+            } else {
+                return {};
+            }
+        }
+        return {};
+    }
+
+    std::vector<const Node*> get_members() const noexcept {
+        if ((m_aml_type == AmlType::TAGGED_UNION) || (m_aml_type == AmlType::TAGGED_STRUCT) || (m_aml_type == AmlType::STRUCT) ||
+            (m_aml_type == AmlType::ROOT)) {
+            std::vector<const Node*> result{};
+            const auto&              members = m_map.at("MEMBERS");
+            for (const auto& member : members.list()) {
+                result.push_back(&member);
+            }
+            return result;
+        }
+        return {};
+    }
+
+    ///////////////////////////////////////////////////////////////
+
+    AmlType aml_type() const noexcept {
+        return m_aml_type;
+    }
+
+    NodeType node_type() const noexcept {
+        return m_node_type;
+    }
+
+    terminal_t value() const noexcept {
+        return m_value;
+    }
+
+    const list_t& list() const noexcept {
+        return m_list;
+    }
+
+    const map_t& map() const noexcept {
+        return m_map;
+    }
+
+   private:
+
+    AmlType    m_aml_type;
+    NodeType   m_node_type;
     terminal_t m_value{};
-    list_t m_list{};
-    map_t m_map{};
+    list_t     m_list{};
+    map_t      m_map{};
 };
 
-inline Node make_pdt(AMLPredefinedType type)  {
+inline Node make_pdt(AMLPredefinedType type) {
     auto res = Node(Node::AmlType::PDT, static_cast<int>(type));
     return res;
 }
 
 inline Node make_referrer(ReferrerType category, const std::string& identifier) {
     Node::map_t map = {
-        {"CATEGORY", Node(Node::AmlType::TERMINAL, static_cast<int>(category))},
-        {"IDENTIFIER", Node(Node::AmlType::TERMINAL, identifier)}
+        { "CATEGORY",   Node(Node::AmlType::TERMINAL, static_cast<int>(category)) },
+        { "IDENTIFIER", Node(Node::AmlType::TERMINAL, identifier)                 }
     };
     auto res = Node(Node::AmlType::REFERRER, map);
     return res;
@@ -116,8 +243,8 @@ using enumerators_t = std::map<std::string, std::uint32_t>;
 
 inline Node make_enumerator(const std::string& name, std::uint32_t value) {
     Node::map_t map = {
-        {"NAME", Node(Node::AmlType::TERMINAL, name)},
-        {"VALUE", Node(Node::AmlType::TERMINAL, value)}
+        { "NAME",  Node(Node::AmlType::TERMINAL, name)  },
+        { "VALUE", Node(Node::AmlType::TERMINAL, value) }
     };
     auto res = Node(Node::AmlType::ENUMERATOR, map);
     return res;
@@ -130,8 +257,8 @@ inline Node make_enumeration(const std::string& name, const enumerators_t& value
         lst.emplace_back(make_enumerator(name, value));
     }
     Node::map_t map = {
-        {"NAME", Node(Node::AmlType::TERMINAL, name)},
-        {"VALUES", Node(Node::AmlType::ENUMERATORS, lst)},
+        { "NAME",   Node(Node::AmlType::TERMINAL,    name) },
+        { "VALUES", Node(Node::AmlType::ENUMERATORS, lst)  },
     };
 
     auto res = Node(Node::AmlType::ENUMERATION, map);
@@ -141,28 +268,24 @@ inline Node make_enumeration(const std::string& name, const enumerators_t& value
 inline Node make_tagged_struct_definition(bool multiple, std::optional<Node> type) {
     Node type_node;
 
-    auto tn = *type;
-
     if (type) {
         type_node = *type;
-    }
-    else {
+    } else {
         type_node = Node();
     }
 
     Node::map_t map = {
-        {"MULTIPLE", Node(Node::AmlType::TERMINAL, multiple)},
-        {"TYPE", type_node},
+        { "MULTIPLE", Node(Node::AmlType::TERMINAL, multiple) },
+        { "TYPE", type_node },
     };
     auto res = Node(Node::AmlType::TAGGED_STRUCT_DEFINITION, map);
     return res;
 }
 
-
 inline Node make_tagged_struct_member(bool multiple, const Node& definition) {
     Node::map_t map = {
-        {"MULTIPLE", Node(Node::AmlType::TERMINAL, multiple)},
-        {"DEFINITION", definition},
+        { "MULTIPLE", Node(Node::AmlType::TERMINAL, multiple) },
+        { "DEFINITION", definition },
     };
     auto res = Node(Node::AmlType::TAGGED_STRUCT_MEMBER, map);
     return res;
@@ -173,15 +296,15 @@ inline Node make_tagged_struct(const std::string& name, std::vector<std::tuple<s
 
     for (const auto& [tag, member] : members) {
         Node::map_t mem_map = {
-            {"TAG", Node(Node::AmlType::TERMINAL, tag)},
-            {"MEMBER", member},
+            { "TAG", Node(Node::AmlType::TERMINAL, tag) },
+            { "MEMBER", member },
         };
         lst.emplace_back(Node(Node::AmlType::MEMBER, mem_map));
     }
 
     Node::map_t map = {
-        {"NAME", Node(Node::AmlType::TERMINAL, name)},
-        {"MEMBERS", Node(Node::AmlType::MEMBERS, lst)},
+        { "NAME",    Node(Node::AmlType::TERMINAL, name) },
+        { "MEMBERS", Node(Node::AmlType::MEMBERS,  lst)  },
     };
     auto res = Node(Node::AmlType::TAGGED_STRUCT, map);
     return res;
@@ -195,8 +318,8 @@ inline Node make_member(const std::vector<std::uint32_t>& array_spec, const Node
     }
 
     Node::map_t map = {
-        {"TYPE", type},
-        {"ARR_SPEC", Node(Node::AmlType::MEMBERS, lst)},
+        { "TYPE", type },
+        { "ARR_SPEC", Node(Node::AmlType::MEMBERS, lst) },
     };
 
     auto res = Node(Node::AmlType::MEMBER, map);
@@ -205,10 +328,10 @@ inline Node make_member(const std::vector<std::uint32_t>& array_spec, const Node
 
 inline Node make_block(const std::string& tag, bool multiple, const Node& type, const Node& member) {
     Node::map_t map = {
-        {"TAG", Node(Node::AmlType::TERMINAL, tag)},
-        {"MULTIPLE", Node(Node::AmlType::TERMINAL, multiple)},
-        {"TYPE", type},
-        {"MEMBER", member},
+        { "TAG", Node(Node::AmlType::TERMINAL, tag) },
+        { "MULTIPLE", Node(Node::AmlType::TERMINAL, multiple) },
+        { "TYPE", type },
+        { "MEMBER", member },
     };
     auto res = Node(Node::AmlType::BLOCK, map);
     return res;
@@ -216,8 +339,8 @@ inline Node make_block(const std::string& tag, bool multiple, const Node& type, 
 
 inline Node make_struct_member(bool multiple, const Node& member) {
     Node::map_t map = {
-        {"MULTIPLE", Node(Node::AmlType::TERMINAL, multiple)},
-        {"MEMBER", member},
+        { "MULTIPLE", Node(Node::AmlType::TERMINAL, multiple) },
+        { "MEMBER", member },
     };
     auto res = Node(Node::AmlType::STRUCT_MEMBER, map);
     return res;
@@ -225,8 +348,8 @@ inline Node make_struct_member(bool multiple, const Node& member) {
 
 inline Node make_struct(const std::string& name, std::vector<Node>& members) {
     Node::map_t map = {
-        {"NAME", Node(Node::AmlType::TERMINAL, name)},
-        {"MEMBERS", Node(Node::AmlType::MEMBERS, members)},
+        { "NAME",    Node(Node::AmlType::TERMINAL, name)    },
+        { "MEMBERS", Node(Node::AmlType::MEMBERS,  members) },
     };
     auto res = Node(Node::AmlType::STRUCT, map);
     return res;
@@ -234,8 +357,8 @@ inline Node make_struct(const std::string& name, std::vector<Node>& members) {
 
 inline Node make_tagged_uinion_member(const std::string& tag, const Node& member) {
     Node::map_t map = {
-        {"TAG", Node(Node::AmlType::TERMINAL, tag)},
-        {"MEMBER", member},
+        { "TAG", Node(Node::AmlType::TERMINAL, tag) },
+        { "MEMBER", member },
     };
     auto res = Node(Node::AmlType::TAGGED_UNION_MEMBER, map);
     return res;
@@ -243,8 +366,8 @@ inline Node make_tagged_uinion_member(const std::string& tag, const Node& member
 
 inline Node make_tagged_uniom(const std::string& name, std::vector<Node>& members) {
     Node::map_t map = {
-        {"NAME", Node(Node::AmlType::TERMINAL, name)},
-        {"MEMBERS", Node(Node::AmlType::MEMBERS, members)},
+        { "NAME",    Node(Node::AmlType::TERMINAL, name)    },
+        { "MEMBERS", Node(Node::AmlType::MEMBERS,  members) },
     };
     auto res = Node(Node::AmlType::TAGGED_UNION, map);
     return res;
@@ -252,7 +375,7 @@ inline Node make_tagged_uniom(const std::string& name, std::vector<Node>& member
 
 inline Node make_root(std::vector<Node>& members) {
     Node::map_t map = {
-        {"MEMBERS", Node(Node::AmlType::MEMBERS, members)},
+        { "MEMBERS", Node(Node::AmlType::MEMBERS, members) },
     };
     auto res = Node(Node::AmlType::ROOT, map);
     return res;
@@ -286,7 +409,7 @@ class Unmarshaller {
                 auto value = m_reader.from_binary< std::uint32_t>();
                 enumerators.emplace(tag, value);
             }
-            return make_enumeration( name, enumerators );
+            return make_enumeration(name, enumerators);
         } else if (disc == "R") {
             return load_referrrer();
         }
@@ -324,7 +447,7 @@ class Unmarshaller {
                 const auto& tag = m_reader.from_binary_str();
                 members.emplace_back(tag, std::move(load_tagged_struct_member()));
             }
-            return make_tagged_struct( name, members );
+            return make_tagged_struct(name, members);
         } else if (disc == "R") {
             return load_referrrer();
         }
@@ -333,8 +456,8 @@ class Unmarshaller {
     Node load_tagged_union() {
         const auto& disc = m_reader.from_binary_str();
         if (disc == "U") {
-            auto                           name       = m_reader.from_binary_str();
-            auto                           tags_count = m_reader.from_binary<std::size_t>();
+            auto              name       = m_reader.from_binary_str();
+            auto              tags_count = m_reader.from_binary<std::size_t>();
             std::vector<Node> members;
 
             // make_tagged_uinion_member
@@ -345,10 +468,8 @@ class Unmarshaller {
 
                 if (dt == "M") {
                     members.emplace_back(make_tagged_uinion_member(tag, load_member()));
-                    //members.emplace_back(tag, std::move(load_member()));
                 } else if (dt == "B") {
                     members.emplace_back(make_tagged_uinion_member(tag, load_block()));
-                    //members.emplace_back(tag, std::move(load_block()));
                 }
             }
             return make_tagged_uniom(name, members);
@@ -396,8 +517,8 @@ class Unmarshaller {
     Node load_struct() {
         const auto& disc = m_reader.from_binary_str();
         if (disc == "S") {
-            auto                      name         = m_reader.from_binary_str();
-            auto                      member_count = m_reader.from_binary<std::size_t>();
+            auto              name         = m_reader.from_binary_str();
+            auto              member_count = m_reader.from_binary<std::size_t>();
             std::vector<Node> members;
 
             for (auto idx = 0UL; idx < member_count; ++idx) {
@@ -427,7 +548,7 @@ class Unmarshaller {
     }
 
     Node run() {
-        auto                     decl_count = m_reader.from_binary<std::size_t>();
+        auto              decl_count = m_reader.from_binary<std::size_t>();
         std::vector<Node> result;
         for (auto idx = 0UL; idx < decl_count; ++idx) {
             const auto& disc1 = m_reader.from_binary_str();
--- pya2l/if_data_parser.cpp
+++ pya2l/if_data_parser.cpp
@@ -19,46 +19,151 @@ Node load_grammar(const std::string& file_name) {
 class IfDataParser {
    public:
 
-    explicit IfDataParser(Node& root) : m_root(root) {
-        // m_grammar.push(&m_root);
+    using token_t = std::optional<std::tuple<int, std::string>>;
+
+    IfDataParser() = delete;
+
+    explicit IfDataParser(const Node& root, const std::string& ifdata_section) : m_ifdata_section(ifdata_section), m_root(root) {
+        m_grammar.push(&m_root);
+        m_input = ANTLRInputStream(m_ifdata_section);
+        m_lexer = std::make_unique<a2llg>(&m_input);
+        consume();
     }
 
-    void parse(const std::string& section) {
-        std::cout << "Parsing section: " << section << std::endl;
-    }
-
-    // find_node -- type
-    const Node* find_block(const std::string& tag) {
-#if 0
-        // auto mr = m_grammar.top();
-        //  auto root  = dynamic_cast<Root*>(mr);
-        auto root  = &m_root;
-        auto nodes = root->nodes();
-
-        for (auto& node : nodes) {
-            auto idx = node.index();
-            if (std::holds_alternative<Block>(node)) {
-                auto block = std::get<Block>(node);
+    void parse() {
+        auto token = current_token();
+        if (token) {
+            auto [type, text] = *token;
+            switch (type) {
+                case a2llg::BEGIN:
+                    consume();
+                    block_type();
+                    break;
+                default:
+                    std::cerr << "Unknown token type: " << type << std::endl;
             }
-#if 0
-            if ((node.type() == Node::NodeType::BLOCK)) {
-                auto res = node.find(tag);
-                if (res != nullptr) {
-                    return res;
-                }
-                Block* block = (Block*)&node;
-                auto   res2  = block->find(tag);
-            }
-#endif
+            parse();
         }
+    }
+
+    token_t next_token() {
+        auto tok        = m_lexer->nextToken();
+        auto token_type = tok->getType();
+        if (token_type == antlr4::Lexer::EOF) {
+            return std::nullopt;
+        }
+        return std::make_tuple<int, std::string>(token_type, tok->getText());
+    }
+
+    token_t current_token() {
+        return m_current_token;
+    }
+
+    void consume() {
+        m_current_token = next_token();
+    }
+
+    const Node* top() const noexcept {
+        if (m_grammar.empty()) {
+            std::cerr << "Stack is empty" << std::endl;
+        }
+        return m_grammar.top();
+    }
+
+    void block_type() {
+        auto token = current_token();
+        if (token) {
+            auto [type, text] = *token;
+            if (type == a2llg::IDENT) {
+                const Node* blk = top()->find_block(text);
+                if (blk) {
+                    m_grammar.push(blk);
+                }
+                consume();
+                const auto& [multiple, member, type] = blk->member_or_type();
+                const auto& map                      = top()->map();
+                auto        tk                       = current_token();
+                if (type) {
+                    m_grammar.push(*type);
+                    switch ((*type)->aml_type()) {
+                        case Node::AmlType::STRUCT:
+                            struct_type();
+                            break;
+                        case Node::AmlType::TAGGED_STRUCT:
+                            tagged_struct_type();
+                            break;
+                        case Node::AmlType::TAGGED_UNION:
+                            tagged_union_type();
+                            break;
+                        case Node::AmlType::ENUMERATION:
+                            enumeration_type();
+                            break;
+                        case Node::AmlType::PDT:
+                            pdt_type();
+                            break;
+                        default:
+                            std::cerr << "Unknown type: " << std::endl;
+                            break;
+                    }
+                } else if (member) {
+                    m_grammar.push(*member);
+                }
+                m_grammar.pop();
+            }
+        }
+    }
+
+    bool match(int token_type, const std::optional<std::string>& value = std::nullopt) {
+#if 0
+        def match(self, token_type, value = None) :
+            ok = self.current_token.type == token_type
+            token_value = self.value(self.current_token)
+            self.consume()
+            if value is None :
+        return ok
+            else:
+        if not ok :
+            return False
+            return token_value == value
 #endif
-        return nullptr;
+    }
+
+    void struct_type() {
+    }
+
+    void tagged_struct_type() {
+    }
+
+    void tagged_union_type() {
+        auto token = current_token();
+        if (token) {
+            auto [tp, text]   = *token;
+            const auto member = top()->find_tag(text);
+            m_grammar.push(member);
+            const auto& [arr_spec, type] = member->get_type();
+            do_type();
+            m_grammar.pop();
+        }
+    }
+
+    void do_type() {
+        const auto tos = top();
+    }
+
+    void enumeration_type() {
+    }
+
+    void pdt_type() {
     }
 
    private:
 
-    Node&                    m_root;
-    std::stack<Node*> m_grammar{};
+    const std::string       m_ifdata_section;
+    ANTLRInputStream        m_input;
+    const Node&             m_root;
+    std::stack<const Node*> m_grammar{};
+    std::unique_ptr<a2llg>  m_lexer;
+    token_t                 m_current_token;
 };
 
 int main() {
@@ -70,34 +175,16 @@ int main() {
 
     auto ifd_lexer = a2llg(&input);
 
-    // std::stack<Node*> gram;
-    // gram.push(&root);
+    auto root = load_grammar("C:/Users/HP/PycharmProjects/pyA2L/pya2l/examples/aml_dump.bin");
 
-    auto root   = load_grammar("C:/Users/HP/PycharmProjects/pyA2L/pya2l/examples/aml_dump.bin");
-    auto parser = IfDataParser(root);
-    parser.parse("/begin IF_DATA ETK\n"
-                 "ADDRESS_MAPPING\n"
-                 "0x10000\n"
-                 "0x10000\n"
-                 "0x1E8\n"
-                 "/end IF_DATA");
-    parser.find_block("IF_DATA");
-    while (true) {
-        auto tok = ifd_lexer.nextToken();
-        auto tp  = tok->getType();
-        std::cout << tok->getText() << " : " << tp << std::endl;
-        if (tok->getType() == antlr4::Lexer::EOF) {
-            break;
-        }
-        if (tok->getType() == a2llg::BEGIN) {
-            auto name = ifd_lexer.nextToken();
-            std::cout << name->getText() << " : " << name->getType() << std::endl;
-            if (name->getType() == a2llg::IDENT) {
-                // auto top = gram.top();
-                //  top->
-            }
-        }
-    }
+    std::string TEXT("/begin IF_DATA ETK\n"
+                     "ADDRESS_MAPPING\n"
+                     "0x10000\n"
+                     "0x10000\n"
+                     "0x1E8\n"
+                     "/end IF_DATA");
+    auto        lex = IfDataParser(root, TEXT);
+    lex.parse();
 
     return 0;
 }
