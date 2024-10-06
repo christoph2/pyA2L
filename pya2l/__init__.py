3d158c98a302e2792ac5836224563defca11b3c6
Christoph Schueler <cpu12.gems@googlemail.com>
2024-10-06 07:23:34 +0300

    Enable position independent code generation

--- pya2l/__init__.py
+++ pya2l/__init__.py
@@ -143,8 +143,7 @@ class DB:
         self.db, listener_result = a2l_parser.parse(filename=filenames.a2l, dbname=str(self._dbfn), encoding=encoding)
         self.session = self.db.session
 
-        self.logger.info("Parsing AML section ...")
-        # aml_parser = parsers.aml(prepro_result=prepro_result)
+        # self.logger.info("Parsing AML section ...")
 
         def parse_aml(file_name: str) -> bytes:
             from pya2l import amlparser_ext
@@ -153,12 +152,9 @@ class DB:
             result: bytes = amlparser_ext.parse_aml(text)
             return text, result
 
-        # from pya2l import amlparser_ext
-        aml_text, aml_parsed = parse_aml(filenames.aml)
-        self.session.add(model.AMLSection(text=aml_text, parsed=aml_parsed))
-        # aml_result = aml_parser.parseFromFile(filename=filenames.aml, dbname=str(self._dbfn), encoding=encoding).listener_result
-        # aml_parsed = pickle.dumps(aml_result)
-        # aml_text = open(filenames.aml).read()
+        # aml_text, aml_parsed = parse_aml(filenames.aml)
+        # self.session.add(model.AMLSection(text=aml_text, parsed=aml_parsed))
+
         """
 
         self.session.add(model.AMLSection(text=aml_text, parsed=aml_parsed))
@@ -171,10 +167,6 @@ class DB:
             self.session.add(item)
         """
         self.session.commit()
-
-        # self.session.add(model.AMLSection(text=aml_text, parsed=aml_parsed))
-
-        # self.session.commit()
         self.logger.info(f"Done [Elapsed time {perf_counter() - start_time:.2f}s].")
         return self.session
 
