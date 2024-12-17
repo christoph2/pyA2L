7b66ef38d4ac7c6e35663589344abbf2278cba1e
Christoph Schueler <cpu12.gems@googlemail.com>
2024-12-17 07:24:15 +0200

    Fix logger for MacOS

--- pya2l/extensions/logger.hpp
+++ pya2l/extensions/logger.hpp
@@ -1,8 +1,21 @@
 #if !defined(__LOGGER_HPP)
 #define __LOGGER_HPP
 
-#define SPDLOG_USE_STD_FORMAT   (1)
+#if !defined(__APPLE__)
+    #define SPDLOG_USE_STD_FORMAT   (1)
+#else
+    #include <cstdio>
+    #if !defined(EOF)
+        #define EOF (-1)
+    #endif
+#endif
 
 #include "spdlog/spdlog.h"
+
+#if defined(__APPLE__)
+    #if defined(EOF)
+        #undef EOF
+    #endif
+#endif
 
 #endif
