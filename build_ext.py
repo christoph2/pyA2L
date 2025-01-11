f115e97b09057cab9e3b6f40214b44f1acecdfc1
Christoph Schueler <cpu12.gems@googlemail.com>
2025-01-11 19:46:25 +0200

    fix tmp dir build

--- build_ext.py
+++ build_ext.py
@@ -178,4 +178,4 @@ def build_extension(debug: bool = False, use_temp_dir=False) -> None:
 if __name__ == "__main__":
     includes = subprocess.getoutput("pybind11-config --cmakedir")  # nosec
     os.environ["pybind11_DIR"] = includes
-    build_extension(use_temp_dir=True)
+    build_extension(use_temp_dir=False)
